67bb156b3a55248e6a5da99e28fdfb7d6ab2bee4
Silverlan <silverlann@gmail.com>
2020-05-17 09:45:37 +0200

    Initial commit

---
+++ include/pr_prosper_vulkan.hpp
@@ -0,0 +1,4 @@
+#ifndef __PR_PROSPER_VULKAN_HPP__
+#define __PR_PROSPER_VULKAN_HPP__
+
+#endif
---
+++ src/pr_prosper_vulkan.cpp
@@ -0,0 +1,15 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include <prosper_vulkan_definitions.hpp>
+#include <vk_context.hpp>
+
+extern "C"
+{
+	DLLPROSPER_VK bool initialize_render_api(const std::string &engineName,bool enableValidation,std::shared_ptr<prosper::IPrContext> &outContext,std::string &errMsg)
+	{
+		outContext = prosper::VlkContext::Create(engineName,enableValidation);
+		return true;
+	}
+};

73938d044b57adff95615181ea9a4f64749a1f10
Silverlan <silverlann@gmail.com>
2025-07-05 16:01:40 +0200

    chore(license): update from MPL‑2.0 to MIT

--- include/pr_prosper_vulkan.hpp
+++ include/pr_prosper_vulkan.hpp
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: (c) 2020 Silverlan <opensource@pragma-engine.com>
+// SPDX-License-Identifier: MIT
+
 #ifndef __PR_PROSPER_VULKAN_HPP__
 #define __PR_PROSPER_VULKAN_HPP__
 

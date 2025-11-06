29c18e2645cdca083a775b7a0d743d3c05263e42
Silverlan <silverlann@gmail.com>
2025-11-06 10:16:18 +0100

    wip

--- include/pr_prosper_vulkan.hpp
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: (c) 2020 Silverlan <opensource@pragma-engine.com>
-// SPDX-License-Identifier: MIT
-
-#ifndef __PR_PROSPER_VULKAN_HPP__
-#define __PR_PROSPER_VULKAN_HPP__
-
-#endif
